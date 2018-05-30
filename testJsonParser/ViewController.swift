4ad7c3f6df8066f0cf1142d32ab1e683c3fa5e55
ohsuyuki <[ohsu@liquidinc.asia]>
2018-05-30 22:00:58 +0900

    create

--- testJsonParser/ViewController.swift
+++ testJsonParser/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let jsonString: String = "{\"id\":1, \"name\":\"Jason\"}"
+        do {
+            guard let json = try JSONSerialization.jsonObject(with: jsonString.data(using: .utf8)!, options: [.allowFragments]) as? [String: Any] else {
+                print("could not parse")
+                return
+            }
+            print("id: \(json["id"]!)")
+            print("name: \(json["name"]!)")
+        } catch let error {
+            print(error)
+        }
     }
-
 
 }
 
